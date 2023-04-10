e5c42ad32a32b2be45b0a8f4929df09ec109ffab
john <joaofxp@hotmail.com>
2023-04-09 22:32:39 -0300

    globo and ccv inseridos

--- gfg_friendshipquotes/gfg_friendshipquotes/spiders/gfg_cars.py
+++ gfg_friendshipquotes/gfg_friendshipquotes/spiders/gfg_cars.py
@@ -5,11 +5,41 @@ import re
 
 carStores = [
     {
+          "storeName":"ccv"
+        , "domain":"ccvcatarinensechevrolet.com.br"
+        , "urlCrawl":"https://www.ccvcatarinensechevrolet.com.br/seminovos?condicao=seminovo&marca=chevrolet,fiat,hyundai,volkswagen&ano=2022,2021,2020,2019,2018&preco=r$%2040.000%20a%20r$%2060.000,r$%2060.000%20a%20r$%2080.000&quilometragem=inferior%20a%2020.000,20.000%20a%2040.000,40.000%20a%2060.000"
+        , "urlNext": None
+        , "buttonNextPage": None
+        , "dadosCarro" : {
+              "listaCarrosXPath":'//*[contains(@class,"vehicle-tile ng-star-inserted")]'
+            , "tituloCarroXPath":'.//p[@class="inventory-title"]/text()'
+            , "kmXPath":None
+            , "anoXPath":'.//p[@class="inventory-title"]/text()'
+            , "valorXPath":'.//p[@class="sub-price"]/text()'
+        }
+    }
+    ,
+    {
+          "storeName":"globo"
+        , "domain":"grupoglobo.com.br"
+        , "urlCrawl":"https://www.grupoglobo.com.br/seminovos"
+        , "urlNext": None
+        , "buttonNextPage": None
+        , "dadosCarro" : {
+              "listaCarrosXPath":'//*[contains(@class, "list_semi") and contains(@class, "col-xs-12 list_semi2")]//div[@class="list_semi" and (contains(.,"Itajaí") or contains(.,"Balneário Camboriú"))]'
+            , "tituloCarroXPath":'.//div[@class="titl_list_semi text_azul2"]//a/text()'
+            , "kmXPath":None
+            , "anoXPath":'.//div[3]//a//span/text()'
+            , "valorXPath":'.//div[@class="valr_list_semi font_bold"]//a/text()[2]'
+        }
+    }
+    ,
+    {
           "storeName":"promenac"
         , "domain":"promenacseminovos.com.br"
         , "urlCrawl":"https://promenacseminovos.com.br/estoque"
         , "urlNext":"https://promenacseminovos.com.br/estoque?_paged="
-        , "buttonNextPage":'//*[contains(text(),"facetwp-page next") and contains(text(),"data-page=")]'
+        , "buttonNextPage": '//*[contains(text(),"facetwp-page next") and contains(text(),"data-page=")]'
         , "dadosCarro" : {
               "listaCarrosXPath":'//*[contains(@class, "elementor-post") and contains(@class, "carros")]'
             , "tituloCarroXPath":'.//div[@id="titulousados"]//h2//a/text()'
@@ -23,17 +53,28 @@ carStores = [
 carIndex = 0
 
 def carregarDadosLojaCarro():
+    global carIndex
+
     carStore = {}
     carStore['listaCarrosXPath'] = carStores[carIndex]["dadosCarro"]["listaCarrosXPath"]
     carStore['tituloCarroXPath'] = carStores[carIndex]["dadosCarro"]["tituloCarroXPath"]
     carStore['kmXPath'] = carStores[carIndex]["dadosCarro"]["kmXPath"]
     carStore['anoXPath'] = carStores[carIndex]["dadosCarro"]["anoXPath"]
     carStore['valorXPath'] = carStores[carIndex]["dadosCarro"]["valorXPath"]
-    carStore['buttonNextPage'] = carStores[carIndex]['buttonNextPage']
+    carStore['buttonNextPage'] = carStores[carIndex]['buttonNextPage'] if carStores[carIndex]['buttonNextPage'] else None
     carStore['urlNext'] = carStores[carIndex]['urlNext']
     carStore['storeName'] = carStores[carIndex]['storeName']
+
     return carStore
 
+def temProximaLoja():
+    global carIndex
+
+    if len(carStores) > carIndex + 1:
+        carIndex += 1
+        return True
+    else:
+        return False
 class GfgCarsSpider(scrapy.Spider):
     name = 'gfg_cars'
     allowed_domains = []
@@ -50,24 +91,39 @@ class GfgCarsSpider(scrapy.Spider):
         carsArray = response.xpath(carStore['listaCarrosXPath'])
 
         for car in carsArray:
-            title = car.xpath(carStore['tituloCarroXPath']).extract_first()
-            km = car.xpath(carStore['kmXPath']).extract_first()
-            km = km.replace('KM ','')
+            title = car.xpath(carStore['tituloCarroXPath']).extract_first().strip()
+            title = re.sub('[0-9]+','',title)
+
+            km = car.xpath(carStore['kmXPath']).extract_first() if carStore['kmXPath'] else ''
+            km = km.replace('KM ','').strip()
+
             ano = car.xpath(carStore['anoXPath']).extract_first()
+            ano = re.findall(r'\d+', ano)
+
             valor = car.xpath(carStore['valorXPath']).extract_first()
-            valor = valor.replace('R$ ','')
+            valor = re.findall('[0-9.,]+',valor)
+
             storeName = carStore['storeName']
 
-            if int(km) < 60000:
+            km = None if km == '' else km
+
+            if km is not None and int(km) < 60000:
                 yield {
                     'Text': title,
+                    'Ano' : ano,
+                    'Valor' : valor,
+                    'Concessionaria': storeName,
                     'KM' : km,
+                }
+            else:
+                yield {
+                    'Text': title,
                     'Ano' : ano,
                     'Valor' : valor,
                     'Concessionaria': storeName,
                 }
 
-        next_page = response.xpath(carStore['buttonNextPage']).get()
+        next_page = response.xpath(carStore['buttonNextPage']).get() if carStore['buttonNextPage'] else None
 
         if next_page is not None:
             # tratativa para promenac, pois carregam o botao em uma tag script e o XPath não consegue extrair de dentro de um string de uma vez só
@@ -81,5 +137,10 @@ class GfgCarsSpider(scrapy.Spider):
                 yield scrapy.Request(url=next_page, callback=self.parse)
             except Exception as e:
                 print(e)
+        else:
+            if temProximaLoja() is True:
+                global carIndex
+                print('novoIndex',carIndex)
+                self.parse()
 
 
